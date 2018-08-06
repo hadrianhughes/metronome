10eaccf655156c41286458d9098a3065ac3ae0e5
Hadrian Hughes <h.hughes@amaze.com>
2018-08-06 23:06:48 +0100

    Basic app set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
@@ -15,5 +17,11 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      inject: 'body'
+    })
+  ]
 };
